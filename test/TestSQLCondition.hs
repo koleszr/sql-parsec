2f0e1b9dc80ba0d45beced9d27a5817d97f7a741
Zoltan Koleszar <koleszr.zoltan@gmail.com>
2019-05-04 18:13:14 +0200

    improve test coverage
    Signed-off-by: Zoltan Koleszar <koleszr.zoltan@gmail.com>

--- test/TestSQLCondition.hs
+++ test/TestSQLCondition.hs
@@ -7,8 +7,12 @@ import Test.Framework.Providers.HUnit
 import Test.HUnit
 import TestUtils
 import Text.ParserCombinators.ReadP
+import Text.Read (readMaybe)
 
-tests = [ testGroup_parseSqlCondition ]
+tests = [ testGroup_parseSqlCondition
+        , testGroup_show_SQLConditionType
+        , testGroup_read_SQLConditionType
+        ]
 
 testGroup_parseSqlCondition =
   testGroup
@@ -62,3 +66,65 @@ test_parseSQLCondition_lte =
     (SQLCondition LTE "age" (NumberValue "27"), ';')
     (parseSQLCondition $ char ';')
     "age <= 27;"
+
+testGroup_show_SQLConditionType =
+  testGroup
+  "SQLConditionType show"
+  [ testCase "Should show equals" test_show_E
+  , testCase "Should show not equals" test_show_NE
+  , testCase "Should show greater than" test_show_GRT
+  , testCase "Should show greater that or equals" test_show_GTE
+  , testCase "Should show less than" test_show_LET
+  , testCase "Should show less than or equals" test_show_LTE
+  ]
+
+test_show_E :: Assertion
+test_show_E = "=" @=? show E
+
+test_show_NE :: Assertion
+test_show_NE = "<>" @=? show NE
+
+test_show_GRT :: Assertion
+test_show_GRT = ">" @=? show GRT
+
+test_show_GTE :: Assertion
+test_show_GTE = ">=" @=? show GTE
+
+test_show_LET :: Assertion
+test_show_LET = "<" @=? show LET
+
+test_show_LTE :: Assertion
+test_show_LTE = "<=" @=? show LTE
+
+testGroup_read_SQLConditionType =
+  testGroup
+  "SQLConditionType read"
+  [ testCase "Should read equals" test_read_E
+  , testCase "Should read not equals" test_read_NE
+  , testCase "Should read greater than" test_read_GRT
+  , testCase "Should read greater that or equals" test_read_GTE
+  , testCase "Should read less than" test_read_LET
+  , testCase "Should read less than or equals" test_read_LTE
+  , testCase "Should fail on invalid input" test_read_invalid
+  ]
+
+test_read_E :: Assertion
+test_read_E = E @=? read "="
+
+test_read_NE :: Assertion
+test_read_NE = NE @=? read "<>"
+
+test_read_GRT :: Assertion
+test_read_GRT = GRT @=? read ">"
+
+test_read_GTE :: Assertion
+test_read_GTE = GTE @=? read ">="
+
+test_read_LET :: Assertion
+test_read_LET = LET @=? read "<"
+
+test_read_LTE :: Assertion
+test_read_LTE = LTE @=? read "<="
+
+test_read_invalid :: Assertion
+test_read_invalid = (Nothing :: Maybe SQLConditionType) @=? (readMaybe "INV")
